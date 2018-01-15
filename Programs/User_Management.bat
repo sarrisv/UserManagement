6212c78d8b9cf694267a2b68b28b1a615e94e3a1
sarrisv <33409202+sarrisv@users.noreply.github.com>
2018-01-15 02:39:26 -0500

    Update User_Management.bat

--- Programs/User_Management.bat
+++ Programs/User_Management.bat
@@ -4,57 +4,32 @@ for /F "usebackq skip=4 delims=" %%L in (`%SystemRoot%\System32\net.exe user`) d
     call :ProcessAccounts "%%L"
 )
 goto :EOF
-
 :ProcessAccounts
 set "Line=%~1"
-
 :NextUser
 set "Name=%Line:~0,20%"
 set "Line=%Line:~20%"
-
 :TrimRight
 if "%Name:~-1%"==" " (
     set "Name=%Name:~0,-1%"
     goto TrimRight
 )
-
-cls
-set /p "auth=Is %Name% an authorized user? (y|n)    "
-if "%auth%"=="y" goto auth
-if "%auth%"=="n" goto delUser
-
-:auth
-cls
-echo Auth
-set /p "type=Is %userName% an Admin? (y|n)  "
-if "%type%"=="y" goto setAdmin
-if "%type%"=="n" goto setNormal
-
-:setAdmin
-cls
-echo setAdmin
-net localgroup administrators "%Name%" /add
-net localgroup users "%Name%" /del
-
-:setNormal
-cls
-echo setNormal
-net localgroup users "%Name%" /add
-net localgroup administrators "%Name%" /del
-
-:delUser
-cls
-echo delUser
-net localgroup users "%Name%" /del
-net localgroup administrators "%Name%" /del
-net user %Name% /del
-
+echo Valid user account name is: %Name%
+if "%Name%"=="Administrator" goto TrimLeft
+if "%Name%"=="Guest" goto TrimLeft
+if "%Name%"=="The command complete" goto EOF
+set /p "auth=Is %Name% an authorized user? y|n  "
+if "%auth%"=="y" set /p "type=Is %Name% an Admin? y|n  "
+if "%auth%"=="n" net user %Name% /del
+if "%type%"=="y" net localgroup administrators %Name% /add
+if "%type%"=="n" (
+  net localgroup users %Name% /add
+  net localgroup administrators %Name% /del
+)
 :TrimLeft
 if "%Line:~0,1%"==" " (
     set "Line=%Line:~1%"
     goto TrimLeft
 )
-
 if not "%Line%"=="" goto NextUser
-
 goto :EOF
